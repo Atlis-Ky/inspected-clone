d9815c53917f70954eec7eac61b7fd5ef17fdaaf
Ky <kylesmith97@outlook.com>
2025-01-16 13:58:33 +0000

    Header done, currency dropdown functional with use state

--- components/Header.jsx
+++ components/Header.jsx
@@ -5,6 +5,8 @@ const Header = () => {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
   const [isPromoBannerVisible, setPromoBannerVisible] = useState(true);
   const [isFadingOut, setFadingOut] = useState(false);
+  const [selectedCurrency, setSelectedCurrency] = useState("GBP");
+
 
   const toggleDropdown = () => {
     setDropdownOpen(!isDropdownOpen);
@@ -17,13 +19,21 @@ const Header = () => {
     }, 500); // Match the duration of the CSS transition
   };
 
+  const selectCurrency = (currency) => {
+    setSelectedCurrency(currency);
+    setDropdownOpen(false);
+  };
+
+
   return (
     <header className="header">
       {/* Promo Banner */}
       {isPromoBannerVisible && (
-        <div className={`promo-banner ${isFadingOut ? 'fade-out' : ''}`}>
+        <div className={`promo-banner ${isFadingOut ? "fade-out" : ""}`}>
           <p>Web Clone made in React.js by Atlis-Ky</p>
-          <button className="promo-banner-close" onClick={closePromoBanner}>✕</button>
+          <button className="promo-banner-close" onClick={closePromoBanner}>
+            ✕
+          </button>
         </div>
       )}
 
@@ -54,51 +64,30 @@ const Header = () => {
         </ul>
 
         {/* Right Section */}
+        
         <div className="header-right">
           {/* Currency Dropdown */}
-          <div
-            className="currency-dropdown"
-            onMouseLeave={() => setDropdownOpen(false)}
-          >
-            <button
-              className="dropdown-button"
-              onClick={toggleDropdown}
-            >
-              GBP <span>▼</span>
-            </button>
-            {isDropdownOpen && (
-              <ul
-                className="dropdown-menu"
-                onMouseEnter={() => setDropdownOpen(true)}
-              >
-                <li>
-                  <a href="#">USD</a>
-                </li>
-                <li>
-                  <a href="#">CAD</a>
-                </li>
-                <li>
-                  <a href="#">AUD</a>
-                </li>
-                <li>
-                  <a href="#">GBP</a>
-                </li>
-                <li>
-                  <a href="#">EUR</a>
-                </li>
-                <li>
-                  <a href="#">JPY</a>
-                </li>
-              </ul>
-            )}
-          </div>
+          <div className="currency-dropdown">
+        <button className="dropdown-button" onClick={toggleDropdown}>
+          {selectedCurrency} <span>▼</span>
+        </button>
+        {isDropdownOpen && (
+          <ul className="dropdown-menu">
+            <li><a href="#" onClick={() => selectCurrency("USD")}>USD</a></li>
+            <li><a href="#" onClick={() => selectCurrency("AUD")}>AUD</a></li>
+            <li><a href="#" onClick={() => selectCurrency("GBP")}>GBP</a></li>
+            <li><a href="#" onClick={() => selectCurrency("EUR")}>EUR</a></li>
+            <li><a href="#" onClick={() => selectCurrency("JPY")}>JPY</a></li>
+          </ul>
+        )}
+      </div>
 
           {/* Login and Cart */}
           <a href="/account" className="icon-user">
-            Login
+            <i className="fas fa-user"></i> Login
           </a>
           <a href="/cart" className="icon-cart">
-            <span>0</span>
+            <i className="fas fa-shopping-cart"></i> <span>0</span>
           </a>
         </div>
       </div>
