0f7168a0c9c3d041fedb97ea26d3108e278f72ca
Ky <kylesmith97@outlook.com>
2025-12-10 20:43:21 +0000

    minor styling updates

--- src/pages/JournalPage.jsx
+++ src/pages/JournalPage.jsx
@@ -5,6 +5,7 @@ import "./JournalPage.css";
 const JournalPage = () => {
   return (
     <div className="journal-page">
+      <h1 className="journal-page-title">JOURNAL</h1>
       <JournalList />
     </div>
   );
